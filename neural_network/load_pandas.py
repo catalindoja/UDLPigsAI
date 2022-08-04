9a06f91737190adbfc11ddfa6d04d45715d7d01f
catalindoja <cdojamihai@gmail.com>
2022-08-04 11:36:09 +0200

    Dataset load using pandas and the append function which is deprecated but at least it works for now

--- neural_network/load_pandas.py
+++ neural_network/load_pandas.py
@@ -1,21 +1,20 @@
 import pandas as pd
+import json
 import glob
 import os
 
-def readFiles(path):
-    files = glob.glob(path)
-    dfs = []
-    for file in files:
-        print(file)
-        data = pd.read_json(file, typ='series')
-        dfs.append(data)
 
-    df = pd.concat(dfs, ignore_index=True)
+def readFiles(directory):
+    df = pd.DataFrame()
+    for filename in os.listdir(directory):
+        file = os.path.join(directory, filename)
+        data = json.load(open(file))
+        df = df.append(data, ignore_index=True)
+
     return df
 
 
-print(os.getcwd())
-df = readFiles('./jsons/C1 - 0Gkut3_1080.json')
-print(df.to_markdown())
+df = readFiles('./jsons/')
+print(df)
 
 
